5e2c41c5064f1a5614d5f016b29ad368980904ac
MiguelABravo <70609667+MiguelABravo@users.noreply.github.com>
2021-02-01 20:13:11 -0800

    Delete Core Assignments/JSON directory

--- Core Assignments/JSON/json_test.js
+++
@@ -1,4 +0,0 @@
-var string = '{"Name": "Miguel Bravo", "Attending": "CSUSB", "Result": "yes"}';
-var obj = JSON.parse(string);
-
-console.log("Is", obj.Name, "attending", obj.Attending, "?", obj.Result);
