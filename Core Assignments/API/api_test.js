9294100a5af602cd0a31b3f61fc63ed7d79b55ad
MiguelABravo <70609667+MiguelABravo@users.noreply.github.com>
2021-02-03 19:09:30 -0800

    Add files via upload

---
+++ Core Assignments/API/api_test.js
@@ -0,0 +1,6 @@
+const fetch = require('node-fetch');
+
+fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400')
+    .then(res => res.json())
+    .then(json => console.log(json));
+
