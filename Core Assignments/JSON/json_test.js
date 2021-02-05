3dac9af82044c29640e4e1fbbc8127b1ae3f3818
MiguelABravo <70609667+MiguelABravo@users.noreply.github.com>
2021-02-04 16:21:38 -0800

    Update json_test.js

--- Core Assignments/JSON/json_test.js
+++ Core Assignments/JSON/json_test.js
@@ -7,7 +7,7 @@ console.log(string);
 // output of objects converted from string
 console.log("Is", obj.Name, "attending", obj.Attending, "?", obj.Result, '\n');
 
-//gaming_consoles output
+// gaming_consoles output
 console.log(gaming_consoles);
 // output of string converted from object
 console.log(JSON.stringify(gaming_consoles));
