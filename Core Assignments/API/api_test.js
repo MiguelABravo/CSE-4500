bf9a36f736371ff23e219887eac9a408a3196432
MiguelABravo <70609667+MiguelABravo@users.noreply.github.com>
2021-02-04 18:21:26 -0800

    Update api_test.js

--- Core Assignments/API/api_test.js
+++ Core Assignments/API/api_test.js
@@ -7,5 +7,5 @@ fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400')
        const obj = JSON.parse(res); // converts string into an object
        console.log(obj.results.sunrise, obj.results.sunset, obj.results.astronomical_twilight_begin, obj.status, '\n'); // using object to output some of the data
        console.log(obj); // outputs all the data from the object
-    })
+    });
 
