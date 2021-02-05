7b1797c9610525beffb405a6e5560578200a575c
MiguelABravo <70609667+MiguelABravo@users.noreply.github.com>
2021-02-04 16:18:59 -0800

    Update json_test.js

--- Core Assignments/JSON/json_test.js
+++ Core Assignments/JSON/json_test.js
@@ -1,13 +1,13 @@
 var string = '{ "Name": "Miguel Bravo", "Attending": "CSUSB", "Result": "yes" }'; // string used to test JSON.parse
 var gaming_consoles = { Microsoft: 'Xbox', Sony: 'Playstation' } // object used to test JSON.stringify
-var obj = JSON.parse(string);
+var obj = JSON.parse(string);// converting string to object
 
 // string output
 console.log(string);
-// objects converted from string output
+// output of objects converted from string
 console.log("Is", obj.Name, "attending", obj.Attending, "?", obj.Result, '\n');
 
 //gaming_consoles output
 console.log(gaming_consoles);
-// string converted from object output
+// output of string converted from object
 console.log(JSON.stringify(gaming_consoles));
