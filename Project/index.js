22af4ed4bc16678c49f46b4866ac0329fccb5242
MiguelABravo <70609667+MiguelABravo@users.noreply.github.com>
2021-03-30 16:02:29 -0700

    Add files via upload

--- Project/index.js
+++ Project/index.js
@@ -1,18 +1,67 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+const rooms = {};
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index', {rooms: rooms});
+});
+
+app.get('/:room', (req, res) => {
+  if ( rooms[req.params.room] == null)
+  {
+    return res.redirect('/');
+  }
+  res.render('room', {room_name: req.params.room});
+});
+
+app.post('/room', (req, res) => {
+  if ( rooms[req.body.room] == null)
+  {
+    rooms[req.body.room] = { users: {} };
+    res.redirect(req.body.room);
+  }
+  else
+  {
+    return res.redirect('/');
+  }
+  io.broadcast.emit('new_room', req.body.room);
 });
 
 /*users message broadcasted to everyone*/
 io.on('connection', (socket) => {
-  socket.on('send_message', msg => {
-    socket.broadcast.emit("user_message", msg);
+  //new user code is not my code, I will change it when I add google authentication
+  socket.on('new-user', (room, name) => {
+    socket.join(room);
+    rooms[room].users[socket.id] = name;
+    socket.to(room).broadcast.emit('user-connected', name);
+  });
+  socket.on('send_message', (room, msg) => {
+    socket.to(room).broadcast.emit("user_message", {msg: msg, name: rooms[room].users[socket.id] });
+  });
+  // I'm going to change disconnect code
+  socket.on('disconnect', () => {
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id]);
+      delete rooms[room].users[socket.id];
+    });
   });
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
-  });
+// I'm going to change getUserRooms code
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
---
+++ Project/public/script.js
@@ -0,0 +1,50 @@
+const socket = io('http://localhost:3000')
+
+const sendf = document.getElementById('send');
+const text = document.getElementById('message_input');
+const mes = document.getElementById('message');
+const roomcon = document.getElementById('roomc');
+
+if (sendf != null) 
+{
+    const name = prompt('Enter name');
+    socket.emit('new-user', room_name, name);
+
+    /*user message sent to client*/
+    sendf.addEventListener('submit', ref => {
+        ref.preventDefault();
+        const msg = text.value;
+        socket.emit('send_message', room_name, msg);
+        Message(`You: ${msg}`);
+        text.value = '';
+        });
+}
+
+socket.on('user_message', data => {
+    Message(`${data.name}: ${data.msg}`);
+    });
+
+/*adds each message to the client*/
+function Message(msg){
+    const mes_ele = document.createElement('div');
+    mes_ele.innerText = msg;
+    mes.append(mes_ele);
+    };
+
+socket.on('new_room', room => {
+    const roome = document.createElement('div');
+    roome.innerText = room;
+    const rooml = document.createElement('a');
+    rooml.href = `/${room}`;
+    rooml.innerText = 'join';
+    roomcon.append(roome);
+    roomcon.append(rooml);
+});
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`);
+  });
+  
+  socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`);
+  });
