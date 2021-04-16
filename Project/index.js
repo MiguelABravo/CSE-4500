5ca3c8c769721efd216980269b91dc2ea5fffd8b
MiguelABravo <70609667+MiguelABravo@users.noreply.github.com>
2021-04-15 18:24:09 -0700

    Update index.js

--- Project/index.js
+++ Project/index.js
@@ -1,42 +1,72 @@
 /*
   Chat app, an app that allows users to create/join chat rooms.
-  I was able to create this app by following watching tutorials.
+  I was able to create this app by watching tutorials.
 */
+
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const local_strat = require ('passport-local').Strategy;
+const { initialize } = require('passport');
+const flash = require('express-flash');
+const session = require ('express-session');
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 const rooms = {};
+const users = [];
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
+});
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
 });
 
 app.get('/login', (req, res) => {
   res.render('login.ejs');
 });
 
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
 app.get('/create', (req, res) => {
   res.render('create.ejs');
 });
 
-app.post('/create', (req, res) => {
+app.post('/create', async (req, res) => {
+  try {
+    const hash_password = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      password: hash_password
+    });
+    res.redirect('/login');
+  }
+  catch{
+    res.redirect('/create');
+  }
+  console.log(users)
+});
 
-})
-
-app.get('/', (req, res) => {
-  res.render('index', {rooms: rooms});
+app.get('/index', (req, res) => {
+  res.render('index.ejs', {rooms: rooms});
 });
 
 app.get('/:room', (req, res) => {
   if ( rooms[req.params.room] == null)
   {
-    return res.redirect('/');
+    return res.redirect('/index');
   }
   res.render('room', {room_name: req.params.room});
 });
@@ -49,7 +79,7 @@ app.post('/room', (req, res) => {
   }
   else
   {
-    return res.redirect('/');
+    return res.redirect('/index');
   }
   io.emit('new_room', req.body.room);
 });
@@ -80,4 +110,42 @@ function getUserRooms(socket) {
     if (room.users[socket.id] != null) names.push(name)
     return names
   }, [])
+}
+
+
+// passport stuff
+app.use(flash());
+app.use(session({
+  secret: Sec,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const Sec = sec;
+module.exports = initialize;
+pass_port(passport, name => users.find(user => user.name === name))
+
+function pass_port(passport, getUserByName){
+  const authenticateUser = (name, password, done) => {
+    const user = getUserByName(name);
+    if (user == null)
+      return done(null, false, { message: 'Error: User does not exist'});
+    
+    try{
+      if (await bcrypt.compare(password, user.password))
+        return done(null, user);
+      else
+        return done(null, false, {message: 'Error: Incorrect password'});
+    }
+    catch(e){
+      return done (e);
+    }
+
+  }
+
+  passport.use(new local_strat({usernameField: 'name'}), authenticateUser);
+  passport.serializeUser((user, done) => { });
+  passport.deserializeUser((id, done) => { });
 }
