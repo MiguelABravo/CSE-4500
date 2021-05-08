ba76edf44d84ed067c2efda5d45532d9963eb6f0
MiguelABravo <70609667+MiguelABravo@users.noreply.github.com>
2021-05-07 18:44:13 -0700

    Update index.js

--- Project/index.js
+++ Project/index.js
@@ -16,6 +16,12 @@ const mongoose = require('mongoose');
 
 //mongodb stuff
 const User = require('./models/user');
+mongoose.connect('mongodb://localhost/chatapp',{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('connected to db'));
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
